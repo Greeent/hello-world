126759eeee7e357e081be2f045ddc66806908aad
Maxim Strizhov <maximSt100@gmail.com>
2021-05-20 20:23:21 +0400

    Hell world app

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3..");
+        System.out.println("Hello world");
+        int  i=1;
+        System.out.println(i);
+        i = i +1;
+        System.out.println(i);
+        i = i +1;
+        System.out.println(i);
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println("sum = " + c);
+        int d = 1;
+        int e = 2;
+        System.out.println("d > e = " + ( d > e) );
+        System.out.println("d < e = " + ( d < e ));
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
